9db6a2acd090de49312fb5c7e7700e48752bc902
Nick Monk <nick@monk.software>
2016-04-03 23:05:56 +0000

    Initialised packages

---
+++ elpa/projectile-20160331.2219/projectile-pkg.el
@@ -0,0 +1,1 @@
+(define-package "projectile" "20160331.2219" "Manage and navigate projects in Emacs easily" (quote ((dash "2.11.0") (pkg-info "0.4"))))
--- init.el
+++ init.el
@@ -1,13 +1,9 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
-
-(defvar my-packages '(better-defaults
-		      projectile))
-
+(defvar my-packages '(better-defaults projectile))
 (dolist (p my-packages)
   (unless (package-installed-p p)
     (package-install p)))
-
 (add-to-list 'custom-theme-load-path "~/.emacs.d/lib/noctilux-theme")
 (load-theme 'noctilux t)
